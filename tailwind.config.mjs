4bc47da2ce564e50dce32ed733ed86f25839d80b
Daniel Rguez Espinosa <drespns@gmail.com>
2024-06-18 13:05:40 +0100

    Extending TAILWIND config in order to animate the border-radius of some of my anchors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				'border-radius-animation': {
+					'0%, 100%': {
+						borderRadius: '30% 70% 70% 30% / 30% 30% 70% 70%'
+					},
+					'50%': {
+						borderRadius: '63% 37% 33% 67% / 30% 53% 47% 70%'
+					},
+				}
+			},
+			animation: {
+				'border-radius': 'border-radius-animation 5s ease-in-out infinite alternate',
+			},
+		},
 	},
 	plugins: [],
 }
